85edc7c875d1dd2909d6299d15879f6756b56408
ruth <ruth@DESKTOP-HOVL7PT>
2020-10-04 15:49:59 +0100

    Test.

--- TM1651.cpp
+++ TM1651.cpp
@@ -41,6 +41,12 @@ static int8_t NumTab[] =
   0x00, 0x63, 0x5c, 0x01, 0x40, 0x08
 }; //numbers 0-9, A-F, special chars
 
+int main(int argc, char *argv[]) {
+
+	TM1651 test = new TM1651(9, 8);
+	test.displayNum(0, 0);
+}
+
 TM1651::TM1651(uint8_t Clk, uint8_t Data)
 {
   Clkpin = Clk;
